106d16eafbdfa3cfad0d0afe6d896d9ee27ba733
mazhongjia <mazhongjia126@126.com>
2020-03-16 20:12:31 +0800

    20200316

---
+++ src/main/java/com/mzj/netty/ssy/_09_nio/NioTest9.java
@@ -0,0 +1,43 @@
+package com.mzj.netty.ssy._09_nio;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+
+/**
+ * 直接缓冲区（DirectByteBuffer）
+ * @Auther: mazhongjia
+ * @Description:
+ */
+public class NioTest9 {
+
+    public static void main(String[] args) throws IOException {
+
+        FileInputStream iggnputStream = new FileInputStream("input2.txt");
+        FileOutputStream outputStream = new FileOutputStream("output2.txt");
+
+        FileChannel inputChannel = inputStream.getChannel();
+        FileChannel outputChannel = outputStream.getChannel();
+
+        ByteBuffer buffer = ByteBuffer.allocateDirect(512);
+
+        while(true){
+            buffer.clear();
+
+            int read = inputChannel.read(buffer);
+
+            System.out.println("read : " + read);
+
+            if(-1 == read){
+                break;
+            }
+            buffer.flip();
+
+            outputChannel.write(buffer);
+        }
+        inputChannel.close();
+        outputChannel.close();
+    }
+}
