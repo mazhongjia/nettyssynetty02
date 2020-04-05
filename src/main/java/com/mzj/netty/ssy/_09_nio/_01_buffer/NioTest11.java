83e1beabdd1cd96fe72c5d1fc8106e02462a84b6
mazhongjia <mazhongjia126@126.com>
2020-04-05 16:57:30 +0800

    20200405

--- src/main/java/com/mzj/netty/ssy/_09_nio/_01_buffer/NioTest11.java
+++ src/main/java/com/mzj/netty/ssy/_09_nio/_01_buffer/NioTest11.java
@@ -13,21 +13,38 @@ import java.nio.channels.FileLock;
  */
 public class NioTest11 {
 
-    public static void main(String[] args) throws IOException {
-        //1、创建RandomAccessFile对象
-        RandomAccessFile randomAccessFile = new RandomAccessFile("NioTest9.txt","rw");
-        //2、通过RandomAccessFile对象获取：NIO的文件Channel对象（1.4版本与NIO一起增加到RandomAccessFile中）
-        FileChannel fileChannel = randomAccessFile.getChannel();
+    public static void main(String[] args) throws IOException, InterruptedException {
+        Thread thread1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+               try{
+                   //1、创建RandomAccessFile对象
+                   RandomAccessFile randomAccessFile = new RandomAccessFile("NioTest9.txt","rw");
+                   //2、通过RandomAccessFile对象获取：NIO的文件Channel对象（1.4版本与NIO一起增加到RandomAccessFile中）
+                   FileChannel fileChannel = randomAccessFile.getChannel();
 
-        //3、获取文件锁对象
-        FileLock fileLock = fileChannel.lock(3,6,true);//第三个参数：true共享锁，false：排他锁，前两个参数：从文件位置3开始，锁定6个字节长度
+                   //3、获取文件锁对象
+                   FileLock fileLock = null;//第三个参数：true共享锁，false：排他锁，前两个参数：从文件位置3开始，锁定6个字节长度
+                   try {
+                       fileLock = fileChannel.lock(3,6,true);
+                   } catch (IOException e) {
+                       e.printStackTrace();
+                   }
+                   System.out.println("Thread1...valid：" + fileLock.isValid());//判断锁有效性（是否成功锁定）
+                   System.out.println("Thread1...lock type : " + fileLock.isShared());//判断锁的类型，是否是共享锁
+                    Thread.sleep(100000);
+                   fileLock.release();//释放锁
 
-        System.out.println("valid：" + fileLock.isValid());//判断锁有效性（是否成功锁定）
-        System.out.println("locak type : " + fileLock.isShared());//判断锁的类型，是否是共享锁
+                   //4、关闭文件
+                   randomAccessFile.close();
+               }catch (Exception e){
+                   e.printStackTrace();
+               }
+            }
+        });
+        thread1.start();
 
-        fileLock.release();//释放锁
 
-        //4、关闭文件
-        randomAccessFile.close();
+        Thread.currentThread().sleep(100000);
     }
 }
---
+++ src/main/java/com/mzj/netty/ssy/_09_nio/_01_buffer/NioTest11_2.java
@@ -0,0 +1,52 @@
+package com.mzj.netty.ssy._09_nio._01_buffer;
+
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.channels.FileChannel;
+import java.nio.channels.FileLock;
+
+/**
+ * 文件锁
+ *
+ * @Auther: mazhongjia
+ * @Description:
+ */
+public class NioTest11_2 {
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+
+        Thread thread2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try{
+                    //1、创建RandomAccessFile对象
+                    RandomAccessFile randomAccessFile = new RandomAccessFile("NioTest9.txt","rw");
+                    //2、通过RandomAccessFile对象获取：NIO的文件Channel对象（1.4版本与NIO一起增加到RandomAccessFile中）
+                    FileChannel fileChannel = randomAccessFile.getChannel();
+
+                    //3、获取文件锁对象
+                    FileLock fileLock = null;//第三个参数：true共享锁，false：排他锁，前两个参数：从文件位置3开始，锁定6个字节长度
+                    try {
+                        System.out.println("JVM2号准备获取锁（无论获取共享还是排他，都不行）");
+                        fileLock = fileChannel.lock(3,6,false);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+
+                    System.out.println("Thread2...valid：" + fileLock.isValid());//判断锁有效性（是否成功锁定）
+                    System.out.println("Thread2...lock type : " + fileLock.isShared());//判断锁的类型，是否是共享锁
+                    Thread.sleep(100000);
+                    fileLock.release();//释放锁
+
+                    //4、关闭文件
+                    randomAccessFile.close();
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
+            }
+        });
+        thread2.start();
+
+        Thread.currentThread().sleep(100000);
+    }
+}
